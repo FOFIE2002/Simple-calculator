862e3211debcaa90c05c328ba86029581357e2ca
FOFIE2002 <fofiefopaelisabeth@gmail.com>
2024-02-14 04:26:38 +0100

    added the toogle property, used css custom property
    added the toogle property, dark and white theme
    used css custom property and wrote some css variables, then wrote some js to toggle

--- script.js
+++ script.js
@@ -66,8 +66,6 @@ function setTheme() {
     const root = document.documentElement;
     const newTheme = root.className === 'dark' ? 'light' : 'dark';
     root.className = newTheme;
-    
-    document.querySelector('.theme-name').textContent = newTheme;
   }
   
   document.querySelector('.theme-toggle').addEventListener('click', setTheme)
